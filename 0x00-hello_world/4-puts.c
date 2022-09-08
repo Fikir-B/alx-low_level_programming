361524923bbf7e239baf77f8a9d08a2672d59d6c
Fikir-B <fikirbeletetilahun@gmail.com>
2022-09-08 14:30:34 -0700

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+*main - prints string
+*the program prints 'Programming is like building a multilingual puzzle'
+*it returns the value : 0
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzel");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");    
+      	return (0);
 }
