d6dce3caecd51e553968573ada36c23b0383ca2b
Fikir-B <fikirbeletetilahun@gmail.com>
2022-09-12 12:22:21 -0700

    add numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -entry point
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d<= '9'; d++)
+		putchar(d);
+	putchar('\n');
+
+	return (0);
+}
