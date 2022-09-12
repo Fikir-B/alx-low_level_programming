f81c4d4491eb064c3d91cf69b8eb69da9b3838bb
Fikir-B <fikirbeletetilahun@gmail.com>
2022-09-12 12:27:11 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -entry point
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'a; low--)
+		putchar(low);
+	putchar('\n');
+
+	return(0);
+}
